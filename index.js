f3770c91809208a064bd9c8ab95a833ed54625dc
Anthony Tineo <anthonytineocabreja3@gmail.com>
2020-03-10 14:50:08 -0400

    Cambios en los enlaces de la imagen y cracion de botones barras para elegir fotos

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 window.onload = function () {
     /*Arreglo que contiene  */
     var images = [
-        '/imagenes/witcher1.jpg',
-        '/imagenes/witcher2.jpg',
-        '/imagenes/witcher3.jpg',
-        '/imagenes/witcher4.jpg',
-        '/imagenes/witcher5.jpg',
-        '/imagenes/witcher6.jpg',
-        '/imagenes/witcher7.jpg'
+        './imagenes/witcher1.jpg',
+        './imagenes/witcher2.jpg',
+        './imagenes/witcher3.jpg',
+        './imagenes/witcher4.jpg',
+        './imagenes/witcher5.jpg',
+        './imagenes/witcher6.jpg',
+        './imagenes/witcher7.jpg'
     ];
 
     var index = 0;
     /*Devuelve lo que contiene la etiqueta botton del html en los dos casos: bottonPrevious, bottonNext*/
     var bottonPrevious = document.querySelector('.previous');
     var bottonNext = document.querySelector('.next');
+    
+    /*Devuelve lo que contienen los botones barras */
+    bars = [
+        document.querySelector('.b1'), 
+        document.querySelector('.b2'),
+        document.querySelector('.b3'),
+        document.querySelector('.b4'),
+        document.querySelector('.b5'),
+        document.querySelector('.b6'),
+        document.querySelector('.b7')
+    ];
+
     /*Devuelve la etiqueta imagen seleccionada*/
     var image = document.querySelector('.image');
+    console.log(image)
     image.style.backgroundImage = 'url('+ images[index] +')';
 
     /*Funcion que cambia la foto en la siguiente posicion e impide que aumenta al acabar de mostrar todas las fotos*/
@@ -46,8 +59,14 @@ window.onload = function () {
     
     /*Funcion que actualiza la etiqueta imagen con la foto nueva, dependiendo de posicionActual*/
     bottonNext.addEventListener("click", ()=>{
-        nextPhotos()
+        nextPhotos();
         image.style.backgroundImage = 'url('+ images[index] +')';
     });
 
+    bars.forEach((bar,index) => {
+        bar.addEventListener("click", ()=>{
+            image.style.backgroundImage = 'url('+ images[index] +')';
+        });
+    });
+    
 }  
